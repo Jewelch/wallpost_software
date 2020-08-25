a92d17b1f3987c0b414c1e4b1bb0bc87e2dc4bee
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-08-25 20:43:04 +0530

    Fixed wrong hr module

--- lib/_shared/constants/base_urls.dart
+++ lib/_shared/constants/base_urls.dart
@@ -2,16 +2,16 @@ class BaseUrls {
   //Sub domains
   static const String _CORE = 'core';
   static const String _TASK_SUB_DOMAIN = 'task';
-  static const String _HR_SUB_DOMAIN = 'task';
+  static const String _HR_SUB_DOMAIN = 'hr';
+
+  //Environments
+  static const String _PRODUCTION = '';
+  static const String _STAGING = 'staging';
+  static const String _TEST = 'test';
 
   //Versions
   static const String _VERSION_2_PATH = 'v2';
   static const String _VERSION_3_PATH = 'v3';
-
-  //Environments
-  static const String _PRODUCTION = '';
-  static const String _STAGING = 'stagingapi';
-  static const String _TEST = 'testapi';
 
   static String generateUrl({String subDomain, String environment, String version}) {
     return 'https://$subDomain.${environment}api.wallpostsoftware.com/api/$version';
