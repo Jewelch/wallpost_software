dca1124fe846fc487ea1d118dbd00daf11e3c1cb
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-08-28 13:00:17 +0530

    Made changes to selected company provider to automatically get the current user

--- lib/company_management/services/selected_company_provider.dart
+++ lib/company_management/services/selected_company_provider.dart
@@ -1,19 +1,22 @@
-import 'package:wallpost/_shared/user_management/entities/user.dart';
+import 'package:wallpost/_shared/user_management/services/current_user_provider.dart';
 import 'package:wallpost/company_management/entities/company.dart';
 import 'package:wallpost/company_management/repositories/company_repository.dart';
 
 class SelectedCompanyProvider {
-  CompanyRepository _companyRepository;
+  final CurrentUserProvider _currentUserProvider;
+  final CompanyRepository _companyRepository;
 
-  SelectedCompanyProvider() {
-    _companyRepository = CompanyRepository();
-  }
+  SelectedCompanyProvider()
+      : _currentUserProvider = CurrentUserProvider(),
+        _companyRepository = CompanyRepository();
 
-  SelectedCompanyProvider.initWith(CompanyRepository companyRepository) {
-    _companyRepository = companyRepository;
-  }
+  SelectedCompanyProvider.initWith(this._currentUserProvider, this._companyRepository);
 
-  Company getSelectedCompanyForUser(User user) {
-    return _companyRepository.getSelectedCompanyForUser(user);
+  Company getSelectCompanyForCurrentUser() {
+    var currentUser = _currentUserProvider.getCurrentUser();
+
+    if (currentUser == null) return null;
+
+    return _companyRepository.getSelectedCompanyForUser(currentUser);
   }
 }
--- test/company_management_tests/service_tests/selected_company_provider_test.dart
+++ test/company_management_tests/service_tests/selected_company_provider_test.dart
@@ -4,6 +4,7 @@ import 'package:wallpost/company_management/entities/company.dart';
 import 'package:wallpost/company_management/repositories/company_repository.dart';
 import 'package:wallpost/company_management/services/selected_company_provider.dart';
 
+import '../../_mocks/mock_current_user_provider.dart';
 import '../../_mocks/mock_user.dart';
 
 class MockCompany extends Mock implements Company {}
@@ -13,16 +14,29 @@ class MockCompanyRepository extends Mock implements CompanyRepository {}
 void main() {
   var mockUser = MockUser();
   var mockCompany = MockCompany();
+  var mockCurrentUserProvider = MockCurrentUserProvider();
   var mockCompanyRepository = MockCompanyRepository();
-  var selectedCompanyProvider = SelectedCompanyProvider.initWith(mockCompanyRepository);
+  var selectedCompanyProvider = SelectedCompanyProvider.initWith(mockCurrentUserProvider, mockCompanyRepository);
 
-  test('getting selected company for a user', () async {
+  test('returns null if there is no current user', () async {
+    when(mockCurrentUserProvider.getCurrentUser()).thenReturn(null);
+
+    var selectedCompany = selectedCompanyProvider.getSelectCompanyForCurrentUser();
+
+    expect(selectedCompany, null);
+    verify(mockCurrentUserProvider.getCurrentUser()).called(1);
+    verifyNever(mockCurrentUserProvider.getCurrentUser());
+  });
+
+  test('getting selected company for current user', () async {
+    when(mockCurrentUserProvider.getCurrentUser()).thenReturn(mockUser);
     when(mockCompanyRepository.getSelectedCompanyForUser(any)).thenReturn(mockCompany);
 
-    var selectedCompany = selectedCompanyProvider.getSelectedCompanyForUser(mockUser);
-    var verificationResult = verify(mockCompanyRepository.getSelectedCompanyForUser(captureAny));
+    var selectedCompany = selectedCompanyProvider.getSelectCompanyForCurrentUser();
 
     expect(selectedCompany, mockCompany);
+    verify(mockCurrentUserProvider.getCurrentUser()).called(1);
+    var verificationResult = verify(mockCompanyRepository.getSelectedCompanyForUser(captureAny));
     verificationResult.called(1);
     expect(verificationResult.captured[0], mockUser);
   });
