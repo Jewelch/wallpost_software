a02df54e6527d4fbceb54c0e0e3715c306dd3390
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-08-28 11:03:25 +0530

    Added company remover

---
+++ test/company_management_tests/service_tests/company_remover_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wallpost/company_management/repositories/company_repository.dart';
+import 'package:wallpost/company_management/services/company_remover.dart';
+
+import '../../_mocks/mock_user.dart';
+
+class MockCompanyRepository extends Mock implements CompanyRepository {}
+
+void main() {
+  var mockUser = MockUser();
+  var mockCompanyRepository = MockCompanyRepository();
+  var companyRemover = CompanyRemover.initWith(mockCompanyRepository);
+
+  test('removing companies for a user', () async {
+    companyRemover.removeCompaniesForUser(mockUser);
+
+    expect(verify(mockCompanyRepository.removeCompaniesForUser(captureAny)).captured.single, mockUser);
+  });
+}
