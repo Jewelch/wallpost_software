1fd1b9261b83a47e1a91103264618fa61cf11fc1
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-09-24 14:10:53 +0530

    Reduced default icon size

--- lib/_common_widgets/buttons/rounded_icon_button.dart
+++ lib/_common_widgets/buttons/rounded_icon_button.dart
@@ -3,16 +3,17 @@ import 'package:flutter_svg/svg.dart';
 import 'package:wallpost/_shared/constants/app_colors.dart';
 
 class RoundedIconButton extends StatelessWidget {
+  static const double WIDTH = 56.0;
   final String iconName;
   final double iconSize;
   final VoidCallback onPressed;
 
-  RoundedIconButton({this.iconName, this.iconSize = 19, this.onPressed});
+  RoundedIconButton({this.iconName, this.iconSize = 18, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 56,
+      width: WIDTH,
       padding: EdgeInsets.all(10),
       child: FlatButton(
         color: AppColors.defaultColor,
@@ -27,6 +28,5 @@ class RoundedIconButton extends StatelessWidget {
         onPressed: onPressed,
       ),
     );
-    ;
   }
 }
