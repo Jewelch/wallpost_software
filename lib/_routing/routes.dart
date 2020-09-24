bc65e5acd3b6fa9f6ba16ef34260849c8dbf5ed5
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-09-24 16:43:21 +0530

    Removed unused routes and refactored

--- lib/_routing/routes.dart
+++ lib/_routing/routes.dart
@@ -4,7 +4,6 @@ import 'package:wallpost/_routing/route_names.dart';
 import 'package:wallpost/authentication/ui/login_screen.dart';
 import 'package:wallpost/company_management/ui/companies_list_screen.dart';
 import 'package:wallpost/dashboard/ui/dashboard_screen.dart';
-import 'package:wallpost/dashboard/ui/left_menu_screen.dart';
 import 'package:wallpost/password_management/ui/change_password_screen.dart';
 import 'package:wallpost/password_management/ui/forgot_password_screen.dart';
 import 'package:wallpost/password_management/ui/forgot_password_success_screen.dart';
@@ -19,29 +18,21 @@ class Routes {
       //Authentication
       RouteNames.login: (BuildContext context) => LoginScreen(),
 
+      //Companies list
+      RouteNames.companiesList: (BuildContext context) => CompaniesListScreen(),
+
+      //Dashboard
+      RouteNames.dashboard: (BuildContext context) => DashboardScreen(),
+
+      //Settings
+      RouteNames.settings: (BuildContext context) => Settings(),
+
       //Password management
       RouteNames.forgotPassword: (BuildContext context) => ForgotPasswordScreen(),
 
       RouteNames.changePassword: (BuildContext context) => ChangePasswordScreen(),
 
       RouteNames.forgotPasswordSuccess: (BuildContext context) => ForgotPasswordSuccessScreen(),
-
-      //Companies list
-      RouteNames.companiesList: (BuildContext context) => CompaniesListScreen(),
-
-      //Companies list
-      RouteNames.dashboard: (BuildContext context) => DashboardScreen(),
-
-      //Settings
-      RouteNames.settings: (BuildContext context) => Settings(),
-
-
-      // Dashboard
-      RouteNames.leftMenu: (BuildContext context) => LeftMenu(),
-
-     
-
-
     };
   }
 }
