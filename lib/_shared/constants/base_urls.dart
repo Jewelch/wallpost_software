32b0e0c3814517178f1fe8e866d802ba756abea9
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2020-10-13 16:39:42 +0530

    Changed to staging

--- lib/_shared/constants/base_urls.dart
+++ lib/_shared/constants/base_urls.dart
@@ -17,7 +17,7 @@ class BaseUrls {
     return 'https://$subDomain.${environment}api.wallpostsoftware.com/api/$version';
   }
 
-  static const String _ENVIRONMENT = _PRODUCTION;
+  static const String _ENVIRONMENT = _STAGING;
 
   static String baseUrlV2() {
     return BaseUrls.generateUrl(
