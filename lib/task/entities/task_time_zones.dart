19e49afaf8a97e06fbf03d5c6b624128339b6dd7
Obaid <mahmed@smartmanagement-its.com>
2020-11-10 13:26:33 +0530

    Added tasks timezones provider

---
+++ lib/task/entities/task_time_zones.dart
@@ -0,0 +1,18 @@
+import 'package:sift/Sift.dart';
+import 'package:wallpost/_shared/exceptions/mapping_exception.dart';
+import 'package:wallpost/_shared/json_serialization_base/json_initializable.dart';
+
+class TaskTimeZones extends JSONInitializable {
+  List<String> _timezones;
+
+  TaskTimeZones.fromJson(Map<String, dynamic> jsonMap) : super.fromJson(jsonMap) {
+    var sift = Sift();
+    try {
+      _timezones = sift.readStringListFromMap(jsonMap, 'timezones');
+    } on SiftException catch (e) {
+      throw MappingException('Failed to cast TaskTimeZones response. Error message - ${e.errorMessage}');
+    }
+  }
+
+  List<String> get timezones => _timezones;
+}
---
+++ test/task_tests/service_tests/task_time_zones_provider_test.dart
@@ -0,0 +1,125 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wallpost/task/constants/task_urls.dart';
+import 'package:wallpost/task/services/task_time_zones_provider.dart';
+
+import '../../_mocks/mock_company.dart';
+import '../../_mocks/mock_company_provider.dart';
+import '../../_mocks/mock_network_adapter.dart';
+import '../mocks.dart';
+
+void main() {
+  List<String> successfulResponse = Mocks.taskTimeZonesResponse;
+  var mockCompany = MockCompany();
+  var mockCompanyProvider = MockCompanyProvider();
+  var mockNetworkAdapter = MockNetworkAdapter();
+  var taskTimeZonesProvider = TaskTimeZonesProvider.initWith(
+    mockCompanyProvider,
+    mockNetworkAdapter,
+  );
+
+  setUpAll(() {
+    when(mockCompany.id).thenReturn('someCompanyId');
+    when(mockCompanyProvider.getSelectedCompanyForCurrentUser()).thenReturn(mockCompany);
+  });
+
+  test('api request is built and executed correctly', () async {
+    Map<String, dynamic> requestParams = {};
+    mockNetworkAdapter.succeed(successfulResponse);
+
+    var _ = await taskTimeZonesProvider.getTimeZones();
+
+    expect(mockNetworkAdapter.apiRequest.url, TaskUrls.getTimeZonesUrl('someCompanyId'));
+    expect(mockNetworkAdapter.apiRequest.parameters, requestParams);
+    expect(mockNetworkAdapter.didCallGet, true);
+  });
+
+  test('throws exception when network adapter fails', () async {
+    mockNetworkAdapter.fail(NetworkFailureException());
+
+    try {
+      var _ = await taskTimeZonesProvider.getTimeZones();
+      fail('failed to throw the network adapter failure exception');
+    } catch (e) {
+      expect(e is NetworkFailureException, true);
+    }
+  });
+
+  test('response is ignored if it is from another session', () async {
+    var didReceiveResponseForTheSecondRequest = false;
+
+    mockNetworkAdapter.succeed(successfulResponse, afterDelayInMilliSeconds: 50);
+    taskTimeZonesProvider.getTimeZones().then((_) {
+      fail('Received the response for the first request. '
+          'This response should be ignored as the session id has changed');
+    });
+
+    mockNetworkAdapter.succeed(successfulResponse);
+    taskTimeZonesProvider.getTimeZones().then((_) {
+      didReceiveResponseForTheSecondRequest = true;
+    });
+
+    await Future.delayed(Duration(milliseconds: 100));
+    expect(didReceiveResponseForTheSecondRequest, true);
+  });
+
+  test('throws InvalidResponseException when response is null', () async {
+    mockNetworkAdapter.succeed(null);
+
+    try {
+      var _ = await taskTimeZonesProvider.getTimeZones();
+      fail('failed to throw InvalidResponseException');
+    } catch (e) {
+      expect(e is InvalidResponseException, true);
+    }
+  });
+
+  test('throws WrongResponseFormatException when response is of the wrong format', () async {
+    mockNetworkAdapter.succeed('wrong response format');
+
+    try {
+      var _ = await taskTimeZonesProvider.getTimeZones();
+      fail('failed to throw WrongResponseFormatException');
+    } catch (e) {
+      expect(e is WrongResponseFormatException, true);
+    }
+  });
+
+  test('success', () async {
+    mockNetworkAdapter.succeed(successfulResponse);
+
+    try {
+      var taskTimeZones = await taskTimeZonesProvider.getTimeZones();
+      expect(taskTimeZones, isNotNull);
+    } catch (e) {
+      fail('failed to complete successfully. exception thrown $e');
+    }
+  });
+
+  test('test loading flag is set to true when the service is executed', () async {
+    mockNetworkAdapter.succeed(successfulResponse);
+
+    taskTimeZonesProvider.getTimeZones();
+
+    expect(taskTimeZonesProvider.isLoading, true);
+  });
+
+  test('test loading flag is reset after success', () async {
+    mockNetworkAdapter.succeed(successfulResponse);
+
+    var _ = await taskTimeZonesProvider.getTimeZones();
+
+    expect(taskTimeZonesProvider.isLoading, false);
+  });
+
+  test('test loading flag is reset after failure', () async {
+    mockNetworkAdapter.fail(InvalidResponseException());
+
+    try {
+      var _ = await taskTimeZonesProvider.getTimeZones();
+      fail('failed to throw exception');
+    } catch (_) {
+      expect(taskTimeZonesProvider.isLoading, false);
+    }
+  });
+}
