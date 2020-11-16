3bca9a6ebab527b4a42bae85f8a0259b9b551fdd
Obaid <mahmed@smartmanagement-its.com>
2020-11-16 16:36:08 +0530

    Added search bar

---
+++ lib/_common_widgets/search_bar/search_bar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:wallpost/_shared/constants/app_images.dart';
+
+class SearchBar extends StatelessWidget {
+  final String hint;
+  final TextEditingController searchTextFieldController;
+  final ValueChanged<String> onSearchTextChanged;
+
+  SearchBar({
+    this.hint,
+    this.searchTextFieldController,
+    this.onSearchTextChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 44,
+      margin: EdgeInsets.symmetric(horizontal: 12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Color.fromRGBO(235, 235, 235, 1.0),
+      ),
+      width: double.infinity,
+      child: Row(
+        children: [
+          SizedBox(width: 12),
+          SvgPicture.asset(
+            AppImages.searchIcon,
+            width: 16,
+            height: 16,
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              controller: searchTextFieldController,
+              maxLines: 1,
+              textCapitalization: TextCapitalization.words,
+              decoration: InputDecoration(
+                hintText: hint,
+                border: InputBorder.none,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    if (searchTextFieldController.text.isEmpty) return;
+                    searchTextFieldController.clear();
+                    onSearchTextChanged('');
+                  },
+                  icon: Icon(
+                    Icons.clear,
+                    size: 18,
+                  ),
+                  color: Colors.grey,
+                ),
+              ),
+              style: TextStyle(fontSize: 16),
+              onChanged: onSearchTextChanged,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/_shared/constants/app_images.dart
+++ lib/_shared/constants/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static final String eyeFilledIcon = "assets/images/eye_filled_icon.svg";
   static final String eyeEmptyIcon = "assets/images/eye_empty_icon.svg";
+  static final String searchIcon = "assets/icons/search_icon.svg";
 }
