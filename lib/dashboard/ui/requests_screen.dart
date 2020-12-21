5abf13404b7483abefcbe12f6307a4ab45b05a17
yehyatt <yehyatt@gmail.com>
2020-12-21 10:04:26 +0300

    requests page

--- lib/dashboard/ui/requests_screen.dart
+++ lib/dashboard/ui/requests_screen.dart
@@ -1,40 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:wallpost/_common_widgets/app_bars/wp_app_bar.dart';
+import 'package:wallpost/_common_widgets/buttons/rounded_icon_button.dart';
 import 'package:wallpost/_routing/route_names.dart';
-import 'package:wallpost/_wp_core/user_management/services/logout_handler.dart';
+import 'package:wallpost/_shared/constants/app_colors.dart';
+import 'package:wallpost/_wp_core/company_management/services/selected_company_provider.dart';
 
 class RequestsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: WPAppBar(
+        title:
+            SelectedCompanyProvider().getSelectedCompanyForCurrentUser().name,
+        leading: RoundedIconButton(
+          iconName: 'assets/icons/back.svg',
+          iconSize: 12,
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
       body: SafeArea(
         child: Container(
           child: Column(
             children: [
-              Text('Requests Screen'),
-              FlatButton(
-                color: Colors.red,
-                child: Text('Logout'),
-                onPressed: () {
-                  LogoutHandler().logout(context);
-                },
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: SizedBox(
+                  height: 40,
+                  child: Row(
+                    children: [
+                      Text('Requests',
+                          style: TextStyle(color: Colors.black, fontSize: 18)),
+                    ],
+                  ),
+                ),
               ),
-              FlatButton(
-                color: Colors.red,
-                child: Text('Go To Tasks'),
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteNames.task);
-                },
-              ),
-              FlatButton(
-                color: Colors.red,
-                child: Text('Go To Leave'),
-                onPressed: () {
-                  Navigator.pushNamed(context, RouteNames.leaveList);
-                },
-              ),
+              Divider(),
+              buildRequestsButton('Task', 'tasks_icon.svg',
+                  () => {Navigator.pushNamed(context, RouteNames.task)}),
+              buildRequestsButton('Leave', 'leave_icon.svg',
+                  () => {Navigator.pushNamed(context, RouteNames.leaveList)}),
+              buildRequestsButton('Expense', 'expense_icon.svg', () => {}),
+              buildRequestsButton('Overtime', 'overtime_icon.svg', () => {}),
+              buildRequestsButton(
+                  'Attendance Adjustment', 'attendance_icon.svg', () => {}),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildRequestsButton(
+      String buttonTitle, String buttonIcon, VoidCallback _onTap) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: _onTap,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: Row(children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: SizedBox(
+                  width: 30,
+                  child: SvgPicture.asset('assets/icons/$buttonIcon',
+                      width: 30, height: 30),
+                ),
+              ),
+              Text(buttonTitle,
+                  style: TextStyle(color: Colors.black, fontSize: 18)),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  backgroundColor: AppColors.defaultColor,
+                  child: Center(
+                    child: Text(
+                      "3",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ]),
+          ),
+          Divider(),
+        ],
       ),
     );
   }
