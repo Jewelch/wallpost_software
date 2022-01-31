6ceb79c628dc9552096ee15f81a81fe7fbe2f831
a3bd2lra7man <a3bd2llah@gmail.com>
2022-01-31 14:29:51 +0300

    inject permissions remover to logout handler

--- lib/_main/services/logout_handler.dart
+++ lib/_main/services/logout_handler.dart
@@ -3,23 +3,28 @@ import 'package:wallpost/_main/ui/views/main_screen.dart';
 import 'package:wallpost/company_list/services/user_companies_remover.dart';
 import 'package:wallpost/_wp_core/user_management/services/current_user_provider.dart';
 import 'package:wallpost/_wp_core/user_management/services/user_remover.dart';
+import 'package:wallpost/permission/services/permissions_remover.dart';
 
 class LogoutHandler {
   final CurrentUserProvider _currentUserProvider;
   final UserRemover _userRemover;
   final UserCompaniesRemover _userCompaniesRemover;
+  final PermissionRemover _permissionRemover;
 
   LogoutHandler()
       : this._currentUserProvider = CurrentUserProvider(),
         this._userRemover = UserRemover(),
+        this._permissionRemover = PermissionRemover(),
         this._userCompaniesRemover = UserCompaniesRemover();
 
-  LogoutHandler.initWith(this._currentUserProvider, this._userRemover, this._userCompaniesRemover);
+  LogoutHandler.initWith(this._currentUserProvider, this._userRemover, this._userCompaniesRemover,
+      this._permissionRemover);
 
   void logout(BuildContext? context) async {
     var user = _currentUserProvider.getCurrentUser();
     _userRemover.removeUser(user);
     _userCompaniesRemover.removeCompaniesForUser(user);
+    _permissionRemover.removePermissions();
     _goToMainScreenAndClearStack(context);
   }
 
--- test/main_tests/service_tests/logout_handler_test.dart
+++ test/main_tests/service_tests/logout_handler_test.dart
@@ -3,6 +3,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:wallpost/_main/services/logout_handler.dart';
 import 'package:wallpost/company_list/services/user_companies_remover.dart';
 import 'package:wallpost/_wp_core/user_management/services/user_remover.dart';
+import 'package:wallpost/permission/services/permissions_remover.dart';
 
 import '../../_mocks/mock_current_user_provider.dart';
 import '../../_mocks/mock_user.dart';
@@ -10,6 +11,8 @@ import '../../_mocks/mock_user.dart';
 class MockUserRemover extends Mock implements UserRemover {}
 
 class MockUserCompaniesRemover extends Mock implements UserCompaniesRemover {}
+
+class MockPermissionRemover extends Mock implements PermissionRemover {}
 
 void main() {
   setUpAll(() {
@@ -20,7 +23,9 @@ void main() {
     var currentUserProvider = MockCurrentUserProvider();
     var userRemover = MockUserRemover();
     var companiesRemover = MockUserCompaniesRemover();
-    var logoutHandler = LogoutHandler.initWith(currentUserProvider, userRemover, companiesRemover);
+    var permissionRemover = MockPermissionRemover();
+    var logoutHandler = LogoutHandler.initWith(
+        currentUserProvider, userRemover, companiesRemover, permissionRemover);
     when(() => currentUserProvider.getCurrentUser()).thenReturn(MockUser());
 
     logoutHandler.logout(null);
@@ -29,9 +34,11 @@ void main() {
       () => currentUserProvider.getCurrentUser(),
       () => userRemover.removeUser(any()),
       () => companiesRemover.removeCompaniesForUser(any()),
+      () => permissionRemover.removePermissions(),
     ]);
     verifyNoMoreInteractions(currentUserProvider);
     verifyNoMoreInteractions(userRemover);
     verifyNoMoreInteractions(companiesRemover);
+    verifyNoMoreInteractions(permissionRemover);
   });
 }
