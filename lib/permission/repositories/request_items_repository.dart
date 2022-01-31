191fc31205e4f244cd43fb61e5db1c7f85cd8c70
a3bd2lra7man <a3bd2llah@gmail.com>
2022-01-31 14:26:25 +0300

    add request items repository

---
+++ lib/permission/repositories/request_items_repository.dart
@@ -0,0 +1,37 @@
+import 'package:wallpost/_shared/local_storage/secure_shared_prefs.dart';
+import 'package:wallpost/permission/entities/request_item.dart';
+
+class RequestItemsRepository {
+  late SecureSharedPrefs _sharedPrefs;
+  final _key = "request_items";
+  List<RequestItem> requestItems = [];
+
+  RequestItemsRepository() : _sharedPrefs = SecureSharedPrefs();
+
+  RequestItemsRepository.initWith(this._sharedPrefs);
+
+  Future saveRequestItemsForEmployee(String companyId, List<RequestItem> requestItems) async {
+    this.requestItems = requestItems;
+    var mappedRequestItem = requestItems.map((item) => item.toReadableString()).toList();
+    Map itemsMap = {companyId: mappedRequestItem};
+    _sharedPrefs.saveMap(_key, itemsMap);
+  }
+
+  Future<List<RequestItem>> getRequestItemsOfCompany(String companyId) async {
+    if (requestItems.isEmpty) {
+      requestItems = await _readRequestItems(companyId);
+    }
+    return requestItems;
+  }
+
+  Future<List<RequestItem>> _readRequestItems(String companyId) async {
+    var itemsMap = await _sharedPrefs.getMap(_key);
+    List mappedItems = itemsMap?[companyId] ?? [];
+    var items = mappedItems.map<RequestItem>((item) => initializeRequestFromString(item)!).toList();
+    return items;
+  }
+
+  Future clearRequestItems() async {
+    await _sharedPrefs.removeMap(_key);
+  }
+}
---
+++ test/permissions_tests/repositories/request_items_repositories_test.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:wallpost/_shared/local_storage/secure_shared_prefs.dart';
+import 'package:wallpost/permission/entities/request_item.dart';
+import 'package:wallpost/permission/repositories/request_items_repository.dart';
+
+class MockSharedPrefs extends Mock implements SecureSharedPrefs {}
+
+void main() {
+  var mockRequestItems = ["leave_request", "expense_request"];
+  var mockSharedPrefs = MockSharedPrefs();
+  RequestItemsRepository requestRepo = RequestItemsRepository.initWith(mockSharedPrefs);
+
+  setUp(() {
+    reset(mockSharedPrefs);
+  });
+
+  test('reading requestItems data when no data is available', () async {
+    when(() => mockSharedPrefs.getMap(any())).thenAnswer((_) => Future.value(null));
+
+    var items = await requestRepo.getRequestItemsOfCompany("companyId");
+
+    expect(items, isEmpty);
+    verifyInOrder([
+      () => mockSharedPrefs.getMap("request_items"),
+    ]);
+    verifyNoMoreInteractions(mockSharedPrefs);
+  });
+
+  test('reading requestItems data  when data is available', () async {
+    var companyId = "1";
+    when(() => mockSharedPrefs.getMap('request_items')).thenAnswer(
+      (_) => Future.value({companyId: mockRequestItems}),
+    );
+
+    var items = await requestRepo.getRequestItemsOfCompany(companyId);
+
+    expect(items.length, 2);
+    expect(items.contains(RequestItem.LeaveRequest), true);
+    expect(items.contains(RequestItem.ExpenseRequest), true);
+    expect(items.contains(RequestItem.Task), false);
+    expect(items.contains(RequestItem.OvertimeRequest), false);
+  });
+
+  test('saving new request items, saves items in memory as well as locally', () async {
+    var companyId = "1";
+    var items = mockRequestItems.map((item) => initializeRequestFromString(item)!).toList();
+
+    await requestRepo.saveRequestItemsForEmployee(companyId, items);
+
+    var verificationResult = verify(() => mockSharedPrefs.saveMap(captureAny(), captureAny()));
+
+    verificationResult.called(1);
+    expect(verificationResult.captured[0], 'request_items');
+    expect(verificationResult.captured[1], {companyId: mockRequestItems});
+    expect(await requestRepo.getRequestItemsOfCompany(companyId), items);
+  });
+
+  test('removing request items', () async {
+    when(() => mockSharedPrefs.getMap('request_items')).thenAnswer(
+      (_) => Future.value(null),
+    );
+
+    await requestRepo.clearRequestItems();
+
+    var verificationResult = verify(() => mockSharedPrefs.removeMap(captureAny()));
+
+    verificationResult.called(1);
+    expect(verificationResult.captured[0], 'request_items');
+  });
+}
