19b4c48b6b9a4b33ea564ea606c16e5be56c6c02
a3bd2lra7man <a3bd2llah@gmail.com>
2022-01-31 14:27:10 +0300

    add helper function to complete request item repository

--- lib/permission/entities/request_item.dart
+++ lib/permission/entities/request_item.dart
@@ -1,8 +1,28 @@
+const taskString = "myportal_create_task";
+const leaveString = "leave_request";
+const expenseString = "expense_request";
+const overTimeString = "overtime_request";
+
 RequestItem? initializeRequestFromString(String itemName) {
-  if (itemName == 'myportal_create_task') return RequestItem.Task;
-  if (itemName == 'leave_request') return RequestItem.LeaveRequest;
-  if (itemName == 'expense_request') return RequestItem.ExpenseRequest;
-  if (itemName == 'overtime_request') return RequestItem.OvertimeRequest;
+  if (itemName == taskString) return RequestItem.Task;
+  if (itemName == leaveString) return RequestItem.LeaveRequest;
+  if (itemName == expenseString) return RequestItem.ExpenseRequest;
+  if (itemName == overTimeString) return RequestItem.OvertimeRequest;
 }
 
 enum RequestItem { Task, ExpenseRequest, LeaveRequest, OvertimeRequest }
+
+extension RequestItemExtension on RequestItem {
+  String toReadableString() {
+    switch (this) {
+      case RequestItem.Task:
+        return taskString;
+      case RequestItem.ExpenseRequest:
+        return expenseString;
+      case RequestItem.LeaveRequest:
+        return leaveString;
+      case RequestItem.OvertimeRequest:
+        return overTimeString;
+    }
+  }
+}
