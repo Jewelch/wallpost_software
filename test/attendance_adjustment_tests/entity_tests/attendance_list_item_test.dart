a8aa63beb3cb91135eb3ff7e21b2152c36697e5d
Ameena <pameena@wallpostdev.com>
2022-01-31 16:19:04 +0530

    attendance adjustment

--- test/attendance_adjustment_tests/entity_tests/attendance_list_item_test.dart
+++ test/attendance_adjustment_tests/entity_tests/attendance_list_item_test.dart
@@ -6,10 +6,14 @@ void main() {
   test('getting values', () async {
     var attendanceListItem = AttendanceListItem.fromJSon(Mocks.attendanceListItemResponse);
 
-    expect(attendanceListItem.status,'PRESENT');
-    expect(attendanceListItem.punchInTime , '09:00' );
-    expect(attendanceListItem.punchOutTime , '06:00' );
+    expect(attendanceListItem.adjustedStatus,'PRESENT');
+    // expect(attendanceListItem.punchInTime , '08:47' );
+    // expect(attendanceListItem.punchOutTime , '18:00' );
     expect(attendanceListItem.date , '2022-01-28' );
+    expect(attendanceListItem.attendanceID , 'Q8pyUr23o8kuGJw' );
+    expect(attendanceListItem.reason , null );
+    expect(attendanceListItem.approvalStatus , 'null' );
+    expect(attendanceListItem.approverName , 'null' );
 
   });
 }
