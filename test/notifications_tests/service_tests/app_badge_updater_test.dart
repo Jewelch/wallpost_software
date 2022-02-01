b66014a8a21be34d71daf18e3c2c2a40994dadb7
yehyatt <yehyatt@gmail.com>
2022-02-01 12:39:14 +0300

    Update notification badge tests

--- test/notifications_tests/service_tests/app_badge_updater_test.dart
+++ test/notifications_tests/service_tests/app_badge_updater_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:wallpost/_common_widgets/app_badge/app_badge.dart';
 import 'package:wallpost/notifications/services/unread_notifications_count_provider.dart';
 
 import '../../_mocks/mock_company.dart';
@@ -9,44 +10,63 @@ import '../../_mocks/mock_network_adapter.dart';
 import '../../main_tests/presenter_tests/main_presenter_test.dart';
 import '../mocks.dart';
 
-class MockUnreadNotificationsCountProvider extends Mock implements UnreadNotificationsCountProvider {}
+class MockUnreadNotificationsCountProvider extends Mock
+    implements UnreadNotificationsCountProvider {}
+
+class MockAppBadge extends Mock implements AppBadge {}
 
 void main() {
-  List<Map<String, dynamic>> successfulResponse = Mocks.notificationsListResponse;
   var mockCompany = MockCompany();
   var currentUserProvider = MockCurrentUserProvider();
   var mockCompanyProvider = MockCompanyProvider();
   var mockNetworkAdapter = MockNetworkAdapter();
-  var unreadNotificationsCountProvider = MockUnreadNotificationsCountProvider();
+  var unreadNotificationsCountProvider = UnreadNotificationsCountProvider.initWith(mockNetworkAdapter);
   var appBadgeUpdater = MockAppBadgeUpdater();
+  var appBadge = MockAppBadge();
 
   void _verifyNoMoreInteractionsOnAllMocks() {
     verifyNoMoreInteractions(appBadgeUpdater);
     verifyNoMoreInteractions(unreadNotificationsCountProvider);
+    verifyNoMoreInteractions(appBadge);
   }
 
   setUp(() {
     when(() => mockCompany.id).thenReturn('selectedCompanyId');
-    when(() => mockCompanyProvider.getSelectedCompanyForCurrentUser()).thenReturn(mockCompany);
+    when(() => mockCompanyProvider.getSelectedCompanyForCurrentUser())
+        .thenReturn(mockCompany);
   });
 
-  test('app count badge is not updated when user is logged out', () async {
+  test('app count badge is not updated when user is logged out', () {
     when(() => currentUserProvider.isLoggedIn()).thenReturn(false);
 
-    await appBadgeUpdater.updateBadgeCount();
+    appBadgeUpdater.updateBadgeCount();
 
     verifyInOrder([
       () => appBadgeUpdater.updateBadgeCount(),
+      () => appBadge.updateAppBadge(any()),
     ]);
     _verifyNoMoreInteractionsOnAllMocks();
   });
-  test('app count badge is updated when user is logged in', () async {
+
+  test('app count badge is updated when user is logged in', ()  {
     when(() => currentUserProvider.isLoggedIn()).thenReturn(true);
 
-    await appBadgeUpdater.updateBadgeCount();
+     appBadgeUpdater.updateBadgeCount();
 
     verifyInOrder([
       () => appBadgeUpdater.updateBadgeCount(),
+      () => appBadge.updateAppBadge(0),
+    ]);
+    _verifyNoMoreInteractionsOnAllMocks();
+  });
+
+  test('app count badge is not updated on exception', ()  {
+    mockNetworkAdapter.fail(NetworkFailureException());
+     appBadgeUpdater.updateBadgeCount();
+
+    verifyInOrder([
+      () => appBadgeUpdater.updateBadgeCount(),
+      () => appBadge.updateAppBadge(0),
     ]);
     _verifyNoMoreInteractionsOnAllMocks();
   });
