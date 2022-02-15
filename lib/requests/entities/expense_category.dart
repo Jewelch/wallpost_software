dbec02bcc4ed36e1919958530e8522a4b689f003
a3bd2lra7man <a3bd2llah@gmail.com>
2022-02-15 07:11:02 +0300

    change expense categories

--- lib/requests/entities/expense_category.dart
+++ lib/requests/entities/expense_category.dart
@@ -5,16 +5,21 @@ import 'package:wallpost/_shared/json_serialization_base/json_initializable.dart
 class ExpenseCategory extends JSONInitializable {
   late final String id;
   late final String name;
+  late final List<ExpenseCategory> subCategories;
+  late final List<String> projects;
 
-  ExpenseCategory.fromJson(Map<String, dynamic> jsonMap)
-      : super.fromJson(jsonMap) {
+  ExpenseCategory.fromJson(Map<String, dynamic> jsonMap) : super.fromJson(jsonMap) {
     try {
       var sift = Sift();
       id = sift.readNumberFromMap(jsonMap, 'id').toString();
       name = sift.readStringFromMap(jsonMap, 'name');
+      subCategories = sift
+          .readMapListFromMap(jsonMap, 'sub_categories')
+          .map((e) => ExpenseCategory.fromJson(e))
+          .toList();
+      projects = sift.readStringListFromMap(jsonMap, 'projects');
     } on SiftException catch (e) {
-      throw MappingException(
-          'Failed to cast Company response. Error message - ${e.errorMessage}');
+      throw MappingException('Failed to cast Company response. Error message - ${e.errorMessage}');
     }
   }
 }
--- test/requests_tests/_mocks/expense_categories_mock.dart
+++ test/requests_tests/_mocks/expense_categories_mock.dart
@@ -1,4 +1,7 @@
 var expenseCategoriesListResponse = [
-  {"id": 1, "name": "Camp Expense", "disable": false},
-  {"id": 2, "name": "Employment Expense", "disable": false},
+  {"id": 1, "name": "Camp Expense", 'sub_categories': [], 'projects': []},
+  {"id": 2, "name": "Employment Expense", 'sub_categories': [], 'projects': []},
 ];
+
+var validExpenseCategoryMap = expenseCategoriesListResponse[0];
+var unValidExpenseCategoryMap = <String, dynamic>{};
