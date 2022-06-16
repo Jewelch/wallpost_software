79279b4bc25b9d3fb63edef5d5d4f39f086a86e7
a3bd2lra7man <51650852+a3bd2lra7man@users.noreply.github.com>
2022-06-16 07:56:34 +0300

    fix api bug

--- lib/expense_requests/services/expense_request_creator.dart
+++ lib/expense_requests/services/expense_request_creator.dart
@@ -52,10 +52,11 @@ class ExpenseRequestCreator {
 
   String _processUploadFileResponse(APIResponse apiResponse) {
     if (apiResponse.data == null) throw InvalidResponseException();
+    print(apiResponse.data);
 
     if (apiResponse.data is! Map<String, dynamic>) throw WrongResponseFormatException();
 
-    if (apiResponse.data['sample_pdf'] == null || apiResponse.data['sample_pdf'] == '')
+    if ((apiResponse.data['sample_pdf'] == null || apiResponse.data['sample_pdf'] == '') && (apiResponse.data['IMG_0004_jpeg'] == null || apiResponse.data['IMG_0004_jpeg'] == ''))
       throw FailedToSaveRequest();
 
     var response = apiResponse.data as Map<String, dynamic>;
