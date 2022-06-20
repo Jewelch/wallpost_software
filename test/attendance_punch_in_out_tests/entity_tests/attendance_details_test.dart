aec62ba9a2835d8d19c45b12a36229136d7b117c
Munavirudheen <munavirckd@gmail.com>
2022-06-20 09:44:35 +0530

    Reformatted attendance details test cases and provider test cases

--- test/attendance_punch_in_out_tests/entity_tests/attendance_details_test.dart
+++ test/attendance_punch_in_out_tests/entity_tests/attendance_details_test.dart
@@ -1,40 +1,48 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:wallpost/attendance_punch_in_out/entities/attendance_details.dart';
-//
-// import '../mocks.dart';
-//
-// void main() {
-//   test('getting time strings', () async {
-//     var attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedOutAttendanceDetailsResponse);
-//
-//     expect(attendanceDetails.attendanceId, 'someAttendanceId');
-//     expect(attendanceDetails.attendanceDetailsId, 'someAttendanceDetailsId');
-//     expect(attendanceDetails.punchInTimeString, '08:00 AM');
-//     expect(attendanceDetails.punchOutTimeString, '05:00 PM');
-//     expect(attendanceDetails.activeBreakStartTimeString, '01:00 PM');
-//   });
-//
-//   test('getting active break id', () async {
-//     var attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceWithActiveBreakResponse);
-//
-//     expect(attendanceDetails.activeBreakId, 'someBreakId');
-//   });
-//
-//   test('different stages of attendance_punch_in_out', () async {
-//     var attendanceDetails = AttendanceDetails.fromJson(Mocks.noAttendanceResponse);
-//     expect(attendanceDetails.isPunchedIn, false);
-//     expect(attendanceDetails.isOnBreak, false);
-//
-//     attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceResponse);
-//     expect(attendanceDetails.isPunchedIn, true);
-//     expect(attendanceDetails.isOnBreak, false);
-//
-//     attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceWithActiveBreakResponse);
-//     expect(attendanceDetails.isPunchedIn, true);
-//     expect(attendanceDetails.isOnBreak, true);
-//
-//     attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedOutAttendanceDetailsResponse);
-//     expect(attendanceDetails.isPunchedIn, false);
-//     expect(attendanceDetails.isOnBreak, false);
-//   });
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wallpost/attendance_punch_in_out/entities/attendance_details.dart';
+
+import '../mocks.dart';
+
+void main() {
+  test('getting time strings', () async {
+    var attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedOutAttendanceDetailsResponse);
+
+    expect(attendanceDetails.attendanceId, 'someAttendanceId');
+    expect(attendanceDetails.attendanceDetailsId, 'someAttendanceDetailsId');
+    expect(attendanceDetails.punchInTimeString, '08:00 AM');
+    expect(attendanceDetails.punchOutTimeString, '05:00 PM');
+    expect(attendanceDetails.activeBreakStartTimeString, '01:00 PM');
+  });
+
+  test('getting active break id', () async {
+    var attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceWithActiveBreakResponse);
+
+    expect(attendanceDetails.activeBreakId, 'someBreakId');
+  });
+
+  test('different stages of attendance_punch_in_out', () async {
+    // var attendanceDetails = AttendanceDetails.fromJson(Mocks.noAttendanceResponse);
+    // expect(attendanceDetails.isPunchedIn, false);
+    // expect(attendanceDetails.canMarkAttendancePermissionFromApp, true);
+    // expect(attendanceDetails.canMarkAttendanceNow, false);
+    // expect(attendanceDetails.isOnBreak, false);
+
+    var attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceResponse);
+    expect(attendanceDetails.isPunchedIn, true);
+    expect(attendanceDetails.canMarkAttendancePermissionFromApp, true);
+    expect(attendanceDetails.canMarkAttendanceNow, true);
+    expect(attendanceDetails.isOnBreak, false);
+
+    attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedInAttendanceWithActiveBreakResponse);
+    expect(attendanceDetails.isPunchedIn, true);
+    expect(attendanceDetails.canMarkAttendancePermissionFromApp, true);
+    expect(attendanceDetails.canMarkAttendanceNow, true);
+    expect(attendanceDetails.isOnBreak, true);
+
+    attendanceDetails = AttendanceDetails.fromJson(Mocks.punchedOutAttendanceDetailsResponse);
+    expect(attendanceDetails.isPunchedIn, false);
+    expect(attendanceDetails.canMarkAttendancePermissionFromApp, true);
+    expect(attendanceDetails.canMarkAttendanceNow, true);
+    expect(attendanceDetails.isOnBreak, false);
+  });
+}
