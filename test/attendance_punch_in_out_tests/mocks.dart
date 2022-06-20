e8f83f37e8bde40368f558ccb825eccc7240aa20
Munavirudheen <munavirckd@gmail.com>
2022-06-20 09:42:58 +0530

    Added punch in allowed details in mock

--- test/attendance_punch_in_out_tests/mocks.dart
+++ test/attendance_punch_in_out_tests/mocks.dart
@@ -1,194 +1,207 @@
 import 'package:random_string/random_string.dart';
 
 class Mocks {
-  static List<Map<String, dynamic>> noAttendanceResponse = [{}];
+  static Map<String, dynamic> noAttendanceResponse = {};
 
-  static List<Map<String, dynamic>> punchedInAttendanceResponse = [
-    {
-      "attendance_details": [
-        {
-          "actual_punch_in": '2020-06-22 08:00:00',
-          "actual_punch_out": null,
-          "attendance_details_id": 'someAttendanceDetailsId',
-          "duration": true,
-          "punch_in": randomString(10),
-          "punch_in_location": {
-            "latitude": randomString(10),
-            "longitude": randomString(10),
+  static Map<String, dynamic> punchedInAttendanceResponse = {
+    "attendance_info": [
+      {
+        "attendance_details": [
+          {
+            "actual_punch_in": '2020-06-22 08:00:00',
+            "actual_punch_out": null,
+            "attendance_details_id": 'someAttendanceDetailsId',
+            "duration": true,
+            "punch_in": randomString(10),
+            "punch_in_location": {
+              "latitude": randomString(10),
+              "longitude": randomString(10),
+            },
+            "punch_out": null,
+            "punch_out_location": null,
+            "punchin_invalid_location": randomString(10),
+            "punchout_invalid_location": randomString(10),
           },
-          "punch_out": null,
-          "punch_out_location": null,
-          "punchin_invalid_location": randomString(10),
-          "punchout_invalid_location": randomString(10),
-        },
-      ],
-      "attendance_id": 'someAttendanceId',
-      "date": randomString(10),
-      "status_arrival": randomString(10),
-      "status_leave": randomString(10),
-      "work_status": randomString(10),
-    },
-  ];
+        ],
+        "attendance_id": 'someAttendanceId',
+        "date": randomString(10),
+        "status_arrival": randomString(10),
+        "status_leave": randomString(10),
+        "work_status": randomString(10),
+      },
+    ],
+    "is_allowed_punchin": {
+      "punch_in_allowed_from_app": true,
+      "remaining_in_min": randomBetween(1000, 5000),
+      "status": true,
+    }
+  };
 
-  static List<Map<String, dynamic>> punchedInAttendanceWithActiveBreakResponse = [
-    {
-      "attendance_details": [
-        {
-          "actual_punch_in": '2020-06-22 08:00:00',
-          "actual_punch_out": null,
-          "attendance_details_id": 'someAttendanceDetailsId',
-          "attendance_intervals": [
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": null,
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
+  static Map<String, dynamic> punchedInAttendanceWithActiveBreakResponse = {
+    "attendance_info": [
+      {
+        "attendance_details": [
+          {
+            "actual_punch_in": '2020-06-22 08:00:00',
+            "actual_punch_out": null,
+            "attendance_details_id": 'someAttendanceDetailsId',
+            "attendance_intervals": [
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": null,
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": 'someBreakId',
               },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": '2020-06-22 13:15:00',
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": randomString(10),
               },
-              "duration": true,
-              "interval_id": 'someBreakId',
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": '2020-06-22 13:15:00',
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": randomString(10),
+              },
+            ],
+            "duration": true,
+            "punch_in": randomString(10),
+            "punch_in_location": {
+              "latitude": randomString(10),
+              "longitude": randomString(10),
             },
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": '2020-06-22 13:15:00',
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "duration": true,
-              "interval_id": randomString(10),
-            },
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": '2020-06-22 13:15:00',
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "duration": true,
-              "interval_id": randomString(10),
-            },
-          ],
-          "duration": true,
-          "punch_in": randomString(10),
-          "punch_in_location": {
-            "latitude": randomString(10),
-            "longitude": randomString(10),
+            "punch_out": null,
+            "punch_out_location": null,
+            "punchin_invalid_location": randomString(10),
+            "punchout_invalid_location": randomString(10),
           },
-          "punch_out": null,
-          "punch_out_location": null,
-          "punchin_invalid_location": randomString(10),
-          "punchout_invalid_location": randomString(10),
-        },
-      ],
-      "attendance_id": 'someAttendanceId',
-      "date": randomString(10),
-      "status_arrival": randomString(10),
-      "status_leave": randomString(10),
-      "work_status": randomString(10),
-    },
-  ];
+        ],
+        "attendance_id": 'someAttendanceId',
+        "date": randomString(10),
+        "status_arrival": randomString(10),
+        "status_leave": randomString(10),
+        "work_status": randomString(10),
+      },
+    ],
+    "is_allowed_punchin": {"status": true, "punch_in_allowed_from_app": true}
+  };
 
-  static List<Map<String, dynamic>> punchedOutAttendanceDetailsResponse = [
-    {
-      "attendance_details": [
-        {
-          "actual_punch_in": '2020-06-22 08:00:00',
-          "actual_punch_out": '2020-06-22 17:00:00',
-          "attendance_details_id": 'someAttendanceDetailsId',
-          "attendance_intervals": [
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": null,
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
+  static Map<String, dynamic> punchedOutAttendanceDetailsResponse = {
+    "attendance_info": [
+      {
+        "attendance_details": [
+          {
+            "actual_punch_in": '2020-06-22 08:00:00',
+            "actual_punch_out": '2020-06-22 17:00:00',
+            "attendance_details_id": 'someAttendanceDetailsId',
+            "attendance_intervals": [
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": null,
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": randomString(10),
               },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": '2020-06-22 13:15:00',
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": randomString(10),
               },
-              "duration": true,
-              "interval_id": randomString(10),
+              {
+                "actual_break_in": '2020-06-22 13:00:00',
+                "actual_break_out": '2020-06-22 13:15:00',
+                "attendance_details_id": randomString(10),
+                "break_in": randomString(10),
+                "break_in_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "break_out": randomString(10),
+                "break_out_location": {
+                  "latitude": randomString(10),
+                  "longitude": randomString(10),
+                },
+                "duration": true,
+                "interval_id": randomString(10),
+              },
+            ],
+            "duration": true,
+            "punch_in": randomString(10),
+            "punch_in_location": {
+              "latitude": randomString(10),
+              "longitude": randomString(10),
             },
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": '2020-06-22 13:15:00',
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "duration": true,
-              "interval_id": randomString(10),
-            },
-            {
-              "actual_break_in": '2020-06-22 13:00:00',
-              "actual_break_out": '2020-06-22 13:15:00',
-              "attendance_details_id": randomString(10),
-              "break_in": randomString(10),
-              "break_in_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "break_out": randomString(10),
-              "break_out_location": {
-                "latitude": randomString(10),
-                "longitude": randomString(10),
-              },
-              "duration": true,
-              "interval_id": randomString(10),
-            },
-          ],
-          "duration": true,
-          "punch_in": randomString(10),
-          "punch_in_location": {
-            "latitude": randomString(10),
-            "longitude": randomString(10),
+            "punch_out": randomString(10),
+            "punch_out_location": randomString(10),
+            "punchin_invalid_location": randomString(10),
+            "punchout_invalid_location": randomString(10),
           },
-          "punch_out": randomString(10),
-          "punch_out_location": randomString(10),
-          "punchin_invalid_location": randomString(10),
-          "punchout_invalid_location": randomString(10),
-        },
-      ],
-      "attendance_id": 'someAttendanceId',
-      "date": randomString(10),
-      "status_arrival": randomString(10),
-      "status_leave": randomString(10),
-      "work_status": randomString(10),
-    },
-  ];
+        ],
+        "attendance_id": 'someAttendanceId',
+        "date": randomString(10),
+        "status_arrival": randomString(10),
+        "status_leave": randomString(10),
+        "work_status": randomString(10),
+      }
+    ],
+    "is_allowed_punchin": {"status": true, "punch_in_allowed_from_app": true}
+  };
 
   static List<Map<String, dynamic>> attendanceReportResponse = [
     {
