b8e6ce91de3148a8bef43a2472812bd79cd4f97c
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-08-04 17:54:37 +0530

    Added error reporter to wpexception

---
+++ lib/_shared/analytics/error_reporter.dart
@@ -0,0 +1,7 @@
+import 'package:wallpost/_shared/exceptions/wp_exception.dart';
+
+class ErrorReporter {
+  void reportError(WPException exception) {
+    //TODO
+  }
+}
--- lib/_shared/exceptions/wp_exception.dart
+++ lib/_shared/exceptions/wp_exception.dart
@@ -1,6 +1,10 @@
+import 'package:wallpost/_shared/analytics/error_reporter.dart';
+
 abstract class WPException implements Exception {
   final String userReadableMessage;
   final String internalErrorMessage;
 
-  WPException(this.userReadableMessage, this.internalErrorMessage);
+  WPException(this.userReadableMessage, this.internalErrorMessage) {
+    ErrorReporter().reportError(this);
+  }
 }
