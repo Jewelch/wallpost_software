5b0196cd9fe19561e37bb76832b261d5905109d9
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-08-07 17:13:24 +0530

    clean up

--- lib/_common_widgets/status_bar_color/status_bar_color_setter.dart
+++ lib/_common_widgets/status_bar_color/status_bar_color_setter.dart
@@ -1,33 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
-import 'package:wallpost/_shared/constants/app_colors.dart';
 
 class StatusBarColorSetter {
-  // static void setColorBasedOnLoginStatus(bool isLoggedIn) {
-  //   if (isLoggedIn) {
-  //     setColorToWhite();
-  //   } else {
-  //     setColorToAppColor();
-  //   }
-  // }
-
-  // static void setColorToAppColor() {
-  //   try {
-  //     var statusBarColor = AppColors.defaultColor;
-  //     FlutterStatusbarcolor.setStatusBarColor(statusBarColor, animate: false);
-  //     FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
-  //   } catch (e) {
-  //     debugPrint(e.toString());
-  //   }
-  // }
-
-  static void setColorToWhite() {
+  void setColorToWhite() {
     try {
       var statusBarColor = Colors.white;
       FlutterStatusbarcolor.setStatusBarColor(statusBarColor, animate: false);
       FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
     } catch (e) {
-      debugPrint(e.toString());
+      //do nothing
     }
   }
 }
