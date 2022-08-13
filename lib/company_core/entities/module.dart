30c1f686e12451a9c374558f77df2edc2e38b0ce
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-08-13 18:01:43 +0530

    Removed task and performance modules

--- lib/company_core/entities/module.dart
+++ lib/company_core/entities/module.dart
@@ -1,19 +1,11 @@
-const _TASK_MODULE_STRING = "task";
 const _HR_MODULE_STRING = "hr";
-const _PERFORMANCE_MODULE_STRING = "performance";
 
 enum Module {
-  Task,
   Hr,
-  Performance,
 }
 
 Module? initializeModuleFromString(String string) {
-  if (string.toLowerCase() == _TASK_MODULE_STRING)
-    return Module.Task;
-  else if (string.toLowerCase() == _HR_MODULE_STRING)
-    return Module.Hr;
-  else if (string.toLowerCase() == _PERFORMANCE_MODULE_STRING) return Module.Performance;
+  if (string.toLowerCase() == _HR_MODULE_STRING) return Module.Hr;
 
   return null;
 }
