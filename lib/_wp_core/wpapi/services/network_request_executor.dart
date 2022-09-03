a1297eecf4011aa877dae78673b1bc9dd55eb320
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-09-04 03:03:07 +0530

    Fixed post with form data

--- lib/_wp_core/wpapi/services/network_request_executor.dart
+++ lib/_wp_core/wpapi/services/network_request_executor.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:dio/dio.dart';
@@ -38,7 +37,7 @@ class NetworkRequestExecutor implements NetworkAdapter {
 
   @override
   Future<APIResponse> postWithFormData(APIRequest apiRequest) async {
-    return executeRequestWithFormData(apiRequest, 'POST');
+    return executeRequestWithFormData(apiRequest);
   }
 
   @override
@@ -70,32 +69,23 @@ class NetworkRequestExecutor implements NetworkAdapter {
     }
   }
 
-  Future<APIResponse> executeRequestWithFormData(APIRequest apiRequest, String method) async {
+  Future<APIResponse> executeRequestWithFormData(APIRequest apiRequest) async {
+    if (await _isConnected() == false) throw NetworkFailureException();
+
     try {
-      HttpClient c = HttpClient();
-      var req = await c.postUrl(Uri.parse(apiRequest.url));
-      apiRequest.headers.forEach((key, value) {
-        req.headers.add(key, value);
-      });
-      req.add(_toFormShape(apiRequest.parameters));
-      var response = await req.close();
-      var stringResponse = await response.transform(utf8.decoder).join();
-      var res = json.decode(stringResponse);
-      return APIResponse(apiRequest, response.statusCode, res, {});
+      var formData = FormData.fromMap(apiRequest.parameters);
+      Response<String> response = await dio.post(
+        apiRequest.url,
+        data: formData,
+        options: Options(
+          headers: apiRequest.headers,
+          validateStatus: (status) => status == 200,
+        ),
+      );
+      return _processResponse(response, apiRequest);
     } on DioError catch (error) {
       throw _processError(error);
     }
-  }
-
-  List<int> _toFormShape(Map map) {
-    var parts = [];
-    map.forEach((key, value) {
-      parts.add('${Uri.encodeQueryComponent(key)}='
-          '${Uri.encodeQueryComponent(json.encode(value))}');
-    });
-    var formData = parts.join('&');
-
-    return utf8.encode(formData); // utf8 encode
   }
 
   Future<bool> _isConnected() {
