9a41591aea3e9f5ae72ca4398ed6ff002afc16c0
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-09-26 19:34:39 +0530

    Removed file

--- lib/attendance/attendance_adjustment_approval_list/ui/view_contracts/attendance_adjustment_approval_view.dart
+++
@@ -1,9 +0,0 @@
-abstract class AttendanceAdjustmentApprovalView {
-  void showLoader();
-
-  void notifyInvalidRejectionReason();
-
-  void onDidPerformActionSuccessfully(String attendanceAdjustmentId);
-
-  void onDidFailToPerformAction(String title, String message);
-}
