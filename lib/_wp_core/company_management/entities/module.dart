8b488f56378997200912bf0e81632a6d61ebba74
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-10-07 07:57:37 +0530

    Added more modules

--- lib/_wp_core/company_management/entities/module.dart
+++ lib/_wp_core/company_management/entities/module.dart
@@ -1,13 +1,19 @@
 const _HR_MODULE_STRING = "hr";
-const _FINANCE_MODULE_STRING = "finance";
+const _CRM_MODULE_STRING = "crm";
+const _RESTAURANT_MODULE_STRING = "restaurant";
+const _RETAIL_MODULE_STRING = "retail";
 
 enum Module {
   Hr,
-  Finance;
+  Crm,
+  Restaurant,
+  Retail;
 
   static Module? initFromString(String string) {
     if (string.toLowerCase() == _HR_MODULE_STRING) return Module.Hr;
-    if (string.toLowerCase() == _FINANCE_MODULE_STRING) return Module.Finance;
+    if (string.toLowerCase() == _CRM_MODULE_STRING) return Module.Crm;
+    if (string.toLowerCase() == _RESTAURANT_MODULE_STRING) return Module.Restaurant;
+    if (string.toLowerCase() == _RETAIL_MODULE_STRING) return Module.Retail;
 
     return null;
   }
@@ -16,8 +22,12 @@ enum Module {
     switch (this) {
       case Module.Hr:
         return "HR";
-      case Module.Finance:
-        return "Finance";
+      case Module.Crm:
+        return "CRM";
+      case Module.Restaurant:
+        return "Restaurant";
+      case Module.Retail:
+        return "Retail";
     }
   }
 }
