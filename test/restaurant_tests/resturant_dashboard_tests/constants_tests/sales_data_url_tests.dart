4d37461e4c4a20bcaba2ac6df363c87283503f9b
a3bd2lra7man <51650852+a3bd2lra7man@users.noreply.github.com>
2022-10-24 14:33:03 +0300

    refactor aggregated sales url

---
+++ test/restaurant_tests/resturant_dashboard_tests/constants_tests/sales_data_url_tests.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wallpost/_shared/constants/base_urls.dart';
+import 'package:wallpost/restaurant/restaurant_dashboard/constants/restaurant_dashboard_urls.dart';
+import 'package:wallpost/restaurant/restaurant_dashboard/entities/sales_break_down_wises.dart';
+
+main() {
+  test("creating sales breakdown url when selected wise is order wise", () {
+    var selectedWise = SalesBreakDownWises.baseOnOrder;
+
+    var url = RestaurantDashboardUrls.getSalesBreakDownsUrl("1", selectedWise);
+
+    expect(url, '${BaseUrls.hrUrlV2()}/companies/1/store/0/consolidated_stats/sales_breakdown/by/order_type');
+  });
+
+  test("creating sales breakdown when selected wise is category wise", () {
+    var selectedWise = SalesBreakDownWises.baseOnCategory;
+
+    var url = RestaurantDashboardUrls.getSalesBreakDownsUrl("1", selectedWise);
+
+    expect(url, '${BaseUrls.hrUrlV2()}/companies/1/store/0/consolidated_stats/sales_breakdown/by/category');
+  });
+
+  test("creating sales breakdown when selected wise is menu item wise", () {
+    var selectedWise = SalesBreakDownWises.baseOnMenu;
+
+    var url = RestaurantDashboardUrls.getSalesBreakDownsUrl("1", selectedWise);
+
+    expect(url, '${BaseUrls.hrUrlV2()}/companies/1/store/0/consolidated_stats/sales_breakdown/by/menu_item');
+  });
+}
