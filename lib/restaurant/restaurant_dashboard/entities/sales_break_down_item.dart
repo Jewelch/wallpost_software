314529bd873c2d88fad91b75a3ecfbb5fbf62b95
a3bd2lra7man <51650852+a3bd2lra7man@users.noreply.github.com>
2022-10-27 09:59:21 +0300

    fix mapping api response bug

--- lib/restaurant/restaurant_dashboard/entities/sales_break_down_item.dart
+++ lib/restaurant/restaurant_dashboard/entities/sales_break_down_item.dart
@@ -4,22 +4,20 @@ import '../../../_shared/exceptions/mapping_exception.dart';
 import '../../../_shared/json_serialization_base/json_initializable.dart';
 
 class SalesBreakDownItem extends JSONInitializable {
-  late String _totalSales;
+  late num _totalSales;
   late String _type;
 
   SalesBreakDownItem.fromJson(dynamic jsonMap) : super.fromJson(jsonMap) {
     final sift = Sift();
     try {
-      _totalSales = sift.readStringFromMap(jsonMap, 'total_sales');
+      _totalSales = sift.readNumberFromMap(jsonMap, 'total_sales');
       _type = sift.readStringFromMap(jsonMap, 'type');
     } on SiftException catch (e) {
-      throw MappingException(
-          'Failed to cast StatisData response. Error message - ${e.errorMessage}');
+      throw MappingException('Failed to cast SalesBreakDownData response. Error message - ${e.errorMessage}');
     }
   }
 
-  double get totalSales => double.parse(_totalSales);
+  double get totalSales => double.parse(_totalSales.toString());
 
   String get type => _type;
 }
-
