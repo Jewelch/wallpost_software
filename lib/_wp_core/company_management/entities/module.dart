642503f83e626ce9b32343f2f02b70f2b0c466d5
Obaid Ahmed Mohammed <obaid@shoutoutsoftware.com>
2022-11-03 14:20:07 +0530

    Fixed order of modules

--- lib/_wp_core/company_management/entities/module.dart
+++ lib/_wp_core/company_management/entities/module.dart
@@ -1,38 +1,38 @@
-const _HR_MODULE_STRING = "hr";
+const _FINANCE_MODULE_STRING = "finance";
 const _CRM_MODULE_STRING = "crm";
 const _RESTAURANT_MODULE_STRING = "restaurant";
 const _RETAIL_MODULE_STRING = "retail";
-const _FINANCE_MODULE_STRING = "finance";
+const _HR_MODULE_STRING = "hr";
 
 enum Module {
-  Hr,
+  Finance,
   Crm,
   Restaurant,
   Retail,
-  Finance;
+  Hr;
 
   static Module? initFromString(String string) {
-    if (string.toLowerCase() == _HR_MODULE_STRING) return Module.Hr;
+    if (string.toLowerCase() == _FINANCE_MODULE_STRING) return Module.Finance;
     if (string.toLowerCase() == _CRM_MODULE_STRING) return Module.Crm;
     if (string.toLowerCase() == _RESTAURANT_MODULE_STRING) return Module.Restaurant;
     if (string.toLowerCase() == _RETAIL_MODULE_STRING) return Module.Retail;
-    if (string.toLowerCase() == _FINANCE_MODULE_STRING) return Module.Finance;
+    if (string.toLowerCase() == _HR_MODULE_STRING) return Module.Hr;
 
     return null;
   }
 
   String toReadableString() {
     switch (this) {
-      case Module.Hr:
-        return "HR";
+      case Module.Finance:
+        return "Finance";
       case Module.Crm:
         return "CRM";
       case Module.Restaurant:
         return "Restaurant";
       case Module.Retail:
         return "Retail";
-      case Module.Finance:
-        return "Finance";
+      case Module.Hr:
+        return "HR";
     }
   }
 }
