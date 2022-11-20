152fa1263ca4e2e18d8e823945670d77a4b5d2fc
a3bd2lra7manM7 <a3bd2llah@gmail.com>
2022-11-20 14:33:42 +0300

    create sales break down item loader

---
+++ lib/restaurant/restaurant_dashboard/ui/views/loader/seles_break_down_loader.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:wallpost/restaurant/restaurant_dashboard/ui/views/loader/restaurant_dashboard_container_loader.dart';
+
+class SalesBreakDownLoader extends StatelessWidget {
+  const SalesBreakDownLoader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RestaurantContainerLoader(
+          height: 60,
+          cornerRadius: 16,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 60,
+          cornerRadius: 16,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 60,
+          cornerRadius: 16,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 60,
+          cornerRadius: 16,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 60,
+          cornerRadius: 16,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 40,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 40,
+          width: double.infinity,
+        ),
+        SizedBox(height: 4),
+
+        RestaurantContainerLoader(
+          height: 40,
+          width: double.infinity,
+        ),
+      ],
+    );
+  }
+}
