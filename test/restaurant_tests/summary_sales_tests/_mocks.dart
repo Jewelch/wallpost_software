362f84291244389ba004e60714746b203f7bcd73
a3bd2lra7manM7 <a3bd2llah@gmail.com>
2023-02-26 01:25:07 +0300

    add tests

---
+++ test/restaurant_tests/summary_sales_tests/_mocks.dart
@@ -0,0 +1,23 @@
+Map<String, dynamic> successfulSalesSummaryResponse = {
+    "summary": {
+      "_gross_sales": "1,574,454",
+      "_net_sales": "1,274,454",
+      "_tax": "0",
+      "_refund": "0",
+      "_discounts": "0"
+    },
+    "data": {
+      "collections": [
+        { "item": "CASH",  "_amount": "459,124","_quantity": "6",},
+        { "item": "CREDIT CARD", "_amount": "115", "_quantity": "6",}
+      ],
+      "order_types": [
+        { "item": "DineIn",  "_total_sales": "1,456",  "_percent": "60%"},
+        { "item": "TakeAway",  "_total_sales": "900",  "_percent": "40.57%"}
+      ],
+      "categories": [
+        { "item": "Burger",  "_amount": "3,118",  "_quantity": "1,600",},
+        { "item": "Coffee",  "_amount": "115",  "_quantity": "6",}
+      ]
+    }
+};
